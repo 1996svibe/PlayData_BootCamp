dd8cb2051ab588a517dfacf25e89829fde5ea506
1996svibe <dndwlscjwo87@gmail.com>
2023-06-15 12:17:19 +0900

    user

--- src/main/java/com/playdata/todos/config/JdbcConnection.java
+++ src/main/java/com/playdata/todos/config/JdbcConnection.java
@@ -9,6 +9,7 @@ public class JdbcConnection {
     private  final String url = "jdbc:mysql://localhost:3306/test2" +
             "?serverTimezone=Asia/Seoul&characterEncoding=UTF-8";
     private  final String username = "root";
+    //
     private  final String password = "1q2w3e4r!!";
 
     public  Connection getJdbc()  {
