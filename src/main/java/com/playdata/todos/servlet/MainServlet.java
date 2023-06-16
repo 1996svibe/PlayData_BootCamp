b1e37d7322924beac1c44a200806a84dd33d529a
1996svibe <dndwlscjwo87@gmail.com>
2023-06-16 17:40:49 +0900

    feature/login4

--- src/main/java/com/playdata/todos/servlet/MainServlet.java
+++ src/main/java/com/playdata/todos/servlet/MainServlet.java
@@ -1,10 +1,10 @@
 package com.playdata.todos.servlet;
+
+import com.playdata.todos.config.History;
 import com.playdata.todos.dao.UserDao;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -12,9 +12,20 @@ public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        req.getRequestDispatcher("views/main.html").forward(req, resp);
-        if(UserDao.me != null){
+        History.setHistory(req, resp);
+        Cookie[] cookies = req.getCookies();
+        String uid = null;
+        String uname = null;
+        for (Cookie c:cookies) {
+            if(c.getName().equals("uid")) uid = c.getValue();
+            if(c.getName().equals("uname")) uname = c.getValue();
+        }
+        HttpSession session = req.getSession();
+        if( session.getAttribute("uname") != null){
+
             resp.setContentType("text/html;charset=UTF-8");
             PrintWriter writer = resp.getWriter();
+            System.out.println(session.getAttribute("uname"));
             writer.println("<!DOCTYPE html>\n" +
                     "<html lang=\"en\">\n" +
                     "<head>\n" +
@@ -22,14 +33,17 @@ public class MainServlet extends HttpServlet {
                     "    <title>main</title>\n" +
                     "</head>\n" +
                     "<body>\n" +
-                    "    <h1>" + UserDao.me.getName() + "님 환영합니다.</h1>\n" +
-                    "    <h1>" + UserDao.me.getName() + "님 가입을 감사드립니다.</h1>\n" +
+                    "    <h1>"  + session.getAttribute("uname") + " 환영합니다.</h1>\n" +
+//                    "    <h1>" + UserDao.me.getCreateAt() + " 가입했습니다.</h1>\n" +
                     "    <img src=\"/img/ffsf.png\">\n" +
+                    "   <a href=\"/back\">back</a>\n" +
+                    "   <a href= \"userupdate\">user update</a>\n" +
                     "</body>\n" +
                     "</html>");
             writer.close();
         }else{
             resp.sendRedirect("/login");
         }
+
     }
 }
