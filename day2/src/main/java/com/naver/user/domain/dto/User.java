ed5d73dbd7066f42e1b6066f9dbfe2932fc6fe2d
1996svibe <dndwlscjwo87@gmail.com>
2023-06-26 10:04:06 +0900

    day2

---
+++ day2/src/main/java/com/naver/user/domain/dto/User.java
@@ -0,0 +1,36 @@
+package com.naver.user.domain.dto;
+
+public class User {
+    public User(String id, String pw) {
+        this.id = id;
+        this.pw = pw;
+    }
+
+    private String id;
+
+    private String pw;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", pw='" + pw + '\'' +
+                '}';
+    }
+}
