343c359f9959ca0b6e498e4703a428b8be3443d9
1996svibe <dndwlscjwo87@gmail.com>
2023-06-26 09:58:52 +0900

    Spring

---
+++ spring-day1/src/main/java/com/naver/user/service/UserService.java
@@ -0,0 +1,23 @@
+package com.naver.user.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public UserService() {
+        System.out.println("----------------- UserService--------------");
+    }
+
+    int count = 0;
+    public int test(){
+        count++;
+        return count;
+    }
+    public int getCount(){
+        System.out.println("----------------- get Count --------------");
+        count++;
+        return 1000;
+    }
+
+}
